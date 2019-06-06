4cb9b454e8d9f2420b78fb685323b36596e1b3e8
kapil gupta <kgupta@sangoma.com>
2019-06-06 14:16:50 +0530

    Fixing FREEPBX-20071

--- Restore.php
+++ Restore.php
@@ -3,7 +3,7 @@ namespace FreePBX\modules\Miscdests;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
 	public function runRestore($jobid){
-		$configs = reset($this->getConfigs());
+		$configs = $this->getConfigs();
 		foreach ($configs as $miscdests) {
 				$this->FreePBX->Miscdests->upsert((int)$miscdests['id'], $miscdests['description'], $miscdests['destdial']);
 		}
