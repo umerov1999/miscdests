f459cb6f0b433b50f065d539dcd2667119743537
nthumann <me@n-thumann.de>
2019-08-17 21:41:49 +0000

    Related to FREEPBX-20414: removed $jobid from runRestore()

--- Restore.php
+++ Restore.php
@@ -2,7 +2,7 @@
 namespace FreePBX\modules\Miscdests;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-	public function runRestore($jobid){
+	public function runRestore(){
 		$configs = $this->getConfigs();
 		foreach ($configs as $miscdests) {
 				$this->FreePBX->Miscdests->upsert((int)$miscdests['id'], $miscdests['description'], $miscdests['destdial']);
