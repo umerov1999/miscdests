deb6b9c5bb98ddc8abc4e5c44d9edbc4b4287909
kapil gupta <kgupta@sangoma.com>
2020-05-11 22:17:54 +0530

    Fixing FREEPBX-21504

--- Miscdests.class.php
+++ Miscdests.class.php
@@ -212,7 +212,7 @@ class Miscdests implements \BMO {
 		debug('*******Update ID: ' . $id . 'description / destdial = ' . $description . ' / ' . $destdial);
 		$q = $db->prepare($sql);
 		$q->execute(array($description,trim($destdial),$id));
-		if(q){
+		if($q){
 			debug($q->rowCount());
 			return $q->rowCount();
 		}
